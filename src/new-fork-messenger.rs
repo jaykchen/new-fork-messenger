04bf4f96c80475c93442d30d87c26ad8c0f5770d
Ji Chen <jaykchen@gmail.com>
2023-03-01 19:52:18 -0500

    octocrab.get inside handler

--- src/new-fork-messenger.rs
+++ src/new-fork-messenger.rs
@@ -1,6 +1,4 @@
-use anyhow::{Error, Result};
-use github_flows::{listen_to_event, octocrab::models::Repository, EventPayload};
-use serde_json::Value;
+use github_flows::{get_octo, listen_to_event, EventPayload};
 use slack_flows::send_message_to_channel;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -11,26 +9,26 @@ pub async fn run() -> anyhow::Result<()> {
 }
 
 async fn handler(payload: EventPayload) {
-    let mut forkee_name: String = "no forkee_name found".to_string();
-    let mut full_name: &str = "no full_name found";
-    let mut visibility: &str = "no visibility found";
-    let mut html_url: String = "no html_url found".to_string();
+    let octocrab = get_octo(None);
 
-    // if let value: Value = serde_json::from_str(payload.ForkEventPayload.clone()) {
-    //     let repo = value.repo.expect("repo doesn't exist on CreateEvent payload");
-    //     full_name = repo.name;
-    //     visibility = if e.public { "public" } else { "private" };
-    // };
+    let repo = octocrab
+        .repos("jaykchen", "vitesse-lite")
+        .get()
+        .await
+        .expect("repo not obtained");
+
+    let full_name = repo.full_name.expect("full_name not obtained");
+    let visibility = repo.visibility.expect("visibility not obtained");
 
     if let EventPayload::ForkEvent(e) = payload {
         let forkee = e.forkee;
-        forkee_name = forkee.owner.expect("no owner field").login;
-        html_url = forkee.html_url.expect("no html_url field").to_string();
-    }
-    let text = format!(
-        "{} forked your {}({})!\n{}",
-        forkee_name, full_name, visibility, html_url
-    );
+        let forkee_name = forkee.owner.expect("no owner field").login;
+        let html_url = forkee.html_url.expect("no html_url field").to_string();
 
-    send_message_to_channel("ik8", "general", text);
+        let text = format!(
+            "{} forked your {}({})!\n{}",
+            forkee_name, full_name, visibility, html_url
+        );
+        send_message_to_channel("ik8", "general", text);
+    }
 }
